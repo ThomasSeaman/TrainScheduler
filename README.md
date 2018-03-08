3dbd4f8f3de8c33a18e8b81236259b37b06c4b80
Thomas Seaman <thomasseaman@dhcp-v066-216.mobile.uci.edu>
2018-03-07 17:48:36 -0800

    Working Besides Moment

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 # Project Start
 # Firebase Done
 # Styling Done
+# Working Besides Moment
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,12 +1,11 @@
-  // Initialize Firebase
-  var config = {
+var config = {
     apiKey: "AIzaSyBXYv4DjhNeGrjP--Oi5oV0NJi4jr8oYKs",
     authDomain: "thomasproject-27.firebaseapp.com",
     databaseURL: "https://thomasproject-27.firebaseio.com",
     projectId: "thomasproject-27",
     storageBucket: "thomasproject-27.appspot.com",
     messagingSenderId: "329218194581"
-  };
+};
 
 firebase.initializeApp(config);
 
@@ -18,46 +17,34 @@ var frequency
 var nextArrival
 var minutesAway
 
-$("#submit").on("click", function (){
+$("#submit").on("click", function () {
     event.preventDefault()
     trainName = $("#trainName-input").val()
-    // console.log("name =" + trainName)
     destination = $("#destination-input").val()
     frequency = $("#frequency-input").val()
     nextArrival = $("#first-train-time-input").val()
-    // console.log("trainName===>",trainName)
     database.ref().push({
-
-        // name:$("#name").val().trim()
-        // name:$("#name").val().trim()
-        // name:$("#name").val().trim()
-        // name:$("#name").val().trim()
-
-        train:trainName,
-        destination:destination,
-        frequency:frequency,
-        arrival:nextArrival
-
+        train: trainName,
+        destination: destination,
+        frequency: frequency,
+        arrival: nextArrival
     })
 })
 
-database.ref().on('child_added', function(snap) {
+database.ref().on('child_added', function (snap) {
     var minutesAwayCalc = 0
-    const {train,destination,frequency,arrival} = snap.val()
-    $("#trainName").append("<div>"+train+"</div>")
-    // console.log(snap.val().train)
-    $("#destination").append("<div>"+destination+"</div>")
-    $("#frequency").append("<div>"+frequency+"</div>")
-    $("#nextArrival").append("<div>"+arrival+"</div>")
-    $("#minutesAway").append("<div>"+minutesAwayCalc+"</div>")
+    const { train, destination, frequency, arrival } = snap.val()
+    $("#trainName").append("<div>" + train + "</div>")
+    $("#destination").append("<div>" + destination + "</div>")
+    $("#frequency").append("<div>" + frequency + "</div>")
+    $("#nextArrival").append("<div>" + arrival + "</div>")
+    $("#minutesAway").append("<div>" + minutesAwayCalc + "</div>")
 })
 
-database.ref().orderByChild('number').limitToLast(1).on('child_added', function(snapshot) {
-    // console.log('hi')
-    // console.log(snapshot.val())
+database.ref().orderByChild('number').limitToLast(1).on('child_added', function (snapshot) {
 })
 
-// var timeArr = tFirstTrain.split(":");
+
 // var trainTime = moment().hours(timeArr[0]).minutes(timeArr[1]);
 // var maxMoment = moment.max(moment(), trainTime);
 
